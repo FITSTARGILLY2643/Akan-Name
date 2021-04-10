8547167d6da5af519a2c87715480038025418a7f
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-04-10 13:12:34 +0300

    Create an array called days to hold days of the week

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ function MakeArray(n) {
         this[i] = 0;
     }
 }
+days = new MakeArray(7);
+days[0] = "Saturday"
+days[1] = "Sunday"
+days[2] = "Monday"
+days[3] = "Tuesday"
+days[4] = "Wednesday"
+days[5] = "Thursday"
+days[6] = "Friday"
