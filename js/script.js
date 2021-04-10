f966ebe4b6dda7c311565bfbccf1ea6284966c0f
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-04-10 13:33:50 +0300

    Create a function to compute the day of birth

--- js/script.js
+++ js/script.js
@@ -50,3 +50,50 @@ female[3] = "Akua"
 female[4] = "Yaa"
 female[5] = "Afua"
 female[6] = "Ama"
+
+//Akan name derivation Function
+function compute(form) {
+    var val1 = parseInt(form.day.value, 10)
+    if ((val1 < 0) || (val1 > 31)) {
+        alert("Enter a valid day")
+    }
+    var val2 = parseInt(form.month.value, 10)
+    if ((val2 < 0) || (val2 > 12)) {
+        alert("Enter a valid month")
+    }
+    var val2x = parseInt(form.month.value, 10)
+    var val3 = parseInt(form.year.value, 10)
+    if (val3 < 1900) {
+        alert("You're that old!")
+    }
+    if (val2 == 1) {
+        val2x = 13;
+        val3 = val3 - 1
+    }
+    if (val2 == 2) {
+        val2x = 14;
+        val3 = val3 - 1
+    }
+    var val4 = parseInt(((val2x + 1) * 3) / 5, 10)
+    var val5 = parseInt(val3 / 4, 10)
+    var val6 = parseInt(val3 / 100, 10)
+    var val7 = parseInt(val3 / 400, 10)
+    var val8 = val1 + (val2x * 2) + val4 + val3 + val5 - val6 + val7 + 2
+    var val9 = parseInt(val8 / 7, 10)
+    var val0 = val8 - (val9 * 7)
+
+    var gender = parseInt(form.gender.value, 10)
+
+    if (gender == 0) {
+        form.result2.value = male[val0]
+
+
+    } else if (gender == 1) {
+        form.result2.value = female[val0]
+    } else {
+        alert("Please choose your Gender!")
+    }
+
+
+
+}
