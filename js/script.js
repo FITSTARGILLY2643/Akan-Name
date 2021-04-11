ba0e10af123771b029eff92ec8fa207c23c5d569
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-04-11 19:15:06 +0300

    Add day field in the form

--- js/script.js
+++ js/script.js
@@ -8,28 +8,29 @@ function MakeArray(n) {
 }
 // Initialize Days of Week Array
 days = new MakeArray(7);
-days[0] = "Saturday"
-days[1] = "Sunday"
-days[2] = "Monday"
-days[3] = "Tuesday"
-days[4] = "Wednesday"
-days[5] = "Thursday"
-days[6] = "Friday"
+days[0] = "Sunday"
+days[1] = "Monday"
+days[2] = "Tuesday"
+days[3] = "Wednesday"
+days[4] = "Thursday"
+days[5] = "Friday"
+days[6] = "Saturday"
+
 
 // Initialize Months Array
 months = new MakeArray(12);
-months[1] = "January"
-months[2] = "February"
-months[3] = "March"
-months[4] = "April"
-months[5] = "May"
-months[6] = "June"
-months[7] = "July"
-months[8] = "August"
-months[9] = "September"
-months[10] = "October"
-months[11] = "November"
-months[12] = "December"
+months[0] = "January"
+months[1] = "February"
+months[2] = "March"
+months[3] = "April"
+months[4] = "May"
+months[5] = "June"
+months[6] = "July"
+months[7] = "August"
+months[8] = "September"
+months[9] = "October"
+months[10] = "November"
+months[11] = "December"
 
 // Initialize Male 's Akan Name Array
 male = new MakeArray(7);
@@ -54,18 +55,28 @@ female[6] = "Ama"
 //Akan name derivation Function
 function compute(form) {
     var val1 = parseInt(form.day.value, 10)
-    if ((val1 < 0) || (val1 > 31)) {
-        alert("Enter a valid day")
-    }
     var val2 = parseInt(form.month.value, 10)
-    if ((val2 < 0) || (val2 > 12)) {
-        alert("Enter a valid month")
+    var val3 = parseInt(form.year.value, 10)
+
+    if ((val1 <= 0) || (val1 > 31)) {
+        alert("Enter a valid day!")
+        return false;
+    }
+
+
+    if ((val2 <= 0) || (val2 > 12)) {
+        alert("Enter a valid month!")
+        return false;
     }
     var val2x = parseInt(form.month.value, 10)
-    var val3 = parseInt(form.year.value, 10)
-    if (val3 < 1900) {
-        alert("You're that old!")
+
+
+    if (val3 < 0) {
+        alert("Enter a valid year!")
+        return false;
     }
+
+
     if (val2 == 1) {
         val2x = 13;
         val3 = val3 - 1
@@ -80,19 +91,23 @@ function compute(form) {
     var val7 = parseInt(val3 / 400, 10)
     var val8 = val1 + (val2x * 2) + val4 + val3 + val5 - val6 + val7 + 2
     var val9 = parseInt(val8 / 7, 10)
-    var val0 = val8 - (val9 * 7)
+    var val0 = (val8 - (val9 * 7)) - 1
 
     var gender = parseInt(form.gender.value, 10)
 
     if (gender == 0) {
+        form.resultDay.value = days[val0]
         form.result2.value = male[val0]
 
 
+
     } else if (gender == 1) {
+        form.resultDay.value = days[val0]
         form.result2.value = female[val0]
     } else {
         alert("Please choose your Gender!")
     }
+
 
 
 
